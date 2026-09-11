4c162a35ca35df9b319e83bcf6b9bd0079d6f526
ASTHA GUPTA <gastha2712@gmail.com>
2026-09-11 23:30:07 +0530

    Time: 1 ms (99.97%), Space: 42.8 MB (26.45%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+       long ans=0;
+        while(x!=0){
+            int r=x%10;
+            ans=ans*10+r;
+            x/=10;
+
+
+        }
+        if(ans>Integer.MAX_VALUE || ans<Integer.MIN_VALUE){
+            return 0;
+        }
+    
+        return (int)ans;
+        
+    }
+}
